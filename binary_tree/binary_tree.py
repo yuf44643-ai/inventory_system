16a45f4705424458e78b7005e4500572fa77454a
core1058 <123lhmnm@gmail.com>
2026-04-09 00:02:19 +0800

    add traversal methods

--- binary_tree/binary_tree.py
+++ binary_tree/binary_tree.py
@@ -7,14 +7,42 @@ class Node:
 
 
 #allocate memory for tree nodes
-firstNode = Node(2)
-secondNode = Node(3)
-thirdNode = Node(4)
-fourthNode = Node(5)
+firstNode = Node("1(root)")
+secondNode = Node(2)
+thirdNode = Node(3)
+fourthNode = Node(4)
+fifthNode = Node(5)
 
 
 #connect binary tree nodes
 firstNode.left = secondNode
 firstNode.right = thirdNode 
 secondNode.left = fourthNode
+secondNode.right = fifthNode
 
+#common Traversals method read all data under and include the node
+#root -> left -> right
+def preOrderTraversal(node):
+    if node is None:
+        return []
+    return [node.data] + preOrderTraversal(node.left) + preOrderTraversal(node.right)
+
+#left -> root -> right
+def inOrderTraversal(node):
+    if node is None:
+        return []
+    return inOrderTraversal(node.left) + [node.data] + inOrderTraversal(node.right)
+
+#left -> right -> root
+def postOrderTraversal(node):
+    if node is None:
+        return []
+    return postOrderTraversal(node.left) + postOrderTraversal(node.right) + [node.data]
+
+#Prints the elements of the traversal list separated by " > "
+def printorder(Traversal):
+    print(" > ".join(map(str, Traversal)))
+
+printorder(preOrderTraversal(firstNode))
+printorder(inOrderTraversal(firstNode))
+printorder(postOrderTraversal(firstNode))
