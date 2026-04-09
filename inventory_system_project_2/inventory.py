aa548380c1b47df72ac169ddad8d8de4bbb7979d
core1058 <123lhmnm@gmail.com>
2026-04-09 19:04:26 +0800

    add classmethod

--- inventory_system_project_2/inventory.py
+++ inventory_system_project_2/inventory.py
@@ -3,20 +3,27 @@ from product import Product
 
 # Inventory management (addition, deletion, update, query)
 class Inventory:
+    numberOfProduct = 0
    # Store all the goods in a dictionary
     def __init__(self):
         self._products: Dict[str, Product] = {}
+
+    @classmethod
+    def addNumberOfProduct(cls):
+        cls.numberOfProduct += 1
 
     # add new products, but the ID cannot be duplicated
     def add_product(self, product: Product) -> None:
         if product.get_product_id() in self._products:
             raise ValueError(f"Product ID {product.get_product_id()} already exists.")
         self._products[product.get_product_id()] = product
+        Inventory.addNumberOfProduct()
         
     # Delete product with ID. Give a reminder if it does not exist
     def remove_product(self, product_id: str) -> None:
         if product_id in self._products:
             del self._products[product_id]
+            Inventory.numberOfProduct -= 1
         else:
             raise KeyError(f"Product ID {product_id} not found.")
 
